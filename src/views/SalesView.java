0062cc4e43395b221a9c4076c5e95dd525a356e4
avp <nichushkin@ukr.net>
2022-04-16 22:13:52 +0300

    HomeWork15 работа с конструктором.

--- src/views/SalesView.java
+++ src/views/SalesView.java
@@ -8,14 +8,14 @@ import java.util.Scanner;
 
 
 // View
-public class SalesView {
+public class SalesView extends Product{
 
     String title;
     String name;
     int quantity;
     double price;
     Scanner scanner;
-    Product model;
+//    Product model;
 
     // Здесь, создайте конструктор данного класса,
     // который в параметре содержит переменную типа модели.
@@ -33,18 +33,18 @@ public class SalesView {
         System.out.print(title);
         // ввода соответствующего значения, валидацью значения
         // через валидатор, установку валидного значения через модель.
-        model.setName(Validator.validateName(scanner));
+        setName(Validator.validateName(scanner));
 
         // Здесь, реализуйте вывод сообщения о необходимсоти
         title = "Введите количество: ";
         System.out.print(title);
         // ввода соответствующего значения, валидацью значения
         // через валидатор, установку валидного значения через модель.
-        quantity = Validator.validateQuantityInput(scanner);
+        setQuantity(Validator.validateQuantityInput(scanner));
 
         title = "Введите цену: ";
         System.out.print(title);
-        price = Validator.validatePriceInput(scanner);
+        setPrice(Validator.validatePriceInput(scanner));
 
         // Здесь, реализуйте вывод сообщения о необходимсоти
         // ввода соответствующего значения, валидацью значения
