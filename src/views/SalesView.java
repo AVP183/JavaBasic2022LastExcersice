3e9c0220dbbc5c3f32b0ef4c25ba9f53b75dfcd9
avp <nichushkin@ukr.net>
2022-04-16 11:59:20 +0300

    HomeWork15 null

--- src/views/SalesView.java
+++ src/views/SalesView.java
@@ -7,14 +7,14 @@ import java.util.Scanner;
 
 
 // View
-public class SalesView{
+public class SalesView extends Product {
 
     String title;
     String name;
     int quantity;
     double price;
     Scanner scanner;
-    Product model;
+//    Product product;
 
     // Здесь, создайте конструктор данного класса,
     // который в параметре содержит переменную типа модели.
@@ -33,18 +33,18 @@ public class SalesView{
         System.out.print(title);
         // ввода соответствующего значения, валидацью значения
         // через валидатор, установку валидного значения через модель.
-        model.setName(Validator.validateName(scanner));
+        setName(Validator.validateName(scanner));
 
         // Здесь, реализуйте вывод сообщения о необходимсоти
         title = "Введите количество: ";
         System.out.print(title);
         // ввода соответствующего значения, валидацью значения
         // через валидатор, установку валидного значения через модель.
-        model.setQuantity(Validator.validateQuantityInput(scanner));
+        setQuantity(Validator.validateQuantityInput(scanner));
 
         title = "Введите цену: ";
         System.out.print(title);
-        model.setPrice(Validator.validatePriceInput(scanner));
+        setPrice(Validator.validatePriceInput(scanner));
 
         // Здесь, реализуйте вывод сообщения о необходимсоти
         // ввода соответствующего значения, валидацью значения
