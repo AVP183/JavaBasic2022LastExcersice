bc3516ccb53ccdfeef0472dac9a7f1a4223219b6
avp <nichushkin@ukr.net>
2022-04-20 20:26:01 +0300

    HomeWork15 Done

--- src/utils/Rounder.java
+++ src/utils/Rounder.java
@@ -1,7 +1,7 @@
 package utils;
 //содержащий статический
 //        метод округления значений.
-public class Rounder {
+public abstract class Rounder {
 
     public double rounderValue(double value) {
         return Math.round(value * 100) / 100.0d;
